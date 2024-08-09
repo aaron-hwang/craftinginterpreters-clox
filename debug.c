8048a451644b4564d8eddccc8ad6f0f2874b4b84
ducc <aaronhwang22@gmail.com>
2024-08-09 13:27:16 -0400

    Added support for a CONSTANT bytecode instruction

---
+++ debug.c
@@ -0,0 +1,48 @@
+//
+// Created by aaron on 8/6/2024.
+//
+
+/**
+ * A file that handles debugging and disassembly of chunks for debugging purposes
+ */
+
+#include <stdio.h>
+#include "debug.h"
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+    printf("== %s ==\n", name);
+
+    for(int offset = 0; offset < chunk->count;) {
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+static int simpleInstruction(const char* name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t constant = chunk->code[offset - 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("' \n");
+
+    return offset + 2;
+}
+
+// Returns an integer representing the offset for the beginning of the next instruction. 
+int disassembleInstruction(Chunk* chunk, int offset) {
+    printf("%04d ", offset);
+
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction) {
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
+    }
+}
---
+++ debug.h
@@ -0,0 +1,13 @@
+//
+// Created by aaron on 8/6/2024.
+//
+
+#ifndef clox_debug_h
+#define clox_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
