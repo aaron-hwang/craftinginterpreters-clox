538385ec23c49e13b63fb881bbc1e9a663586c2b
ducc <aaronhwang22@gmail.com>
2024-08-15 15:28:24 -0400

    Added a vm to support executing bytecode

--- main/chunk.h
+++ main/chunk.h
@@ -11,6 +11,11 @@
 typedef enum {
     OP_RETURN,
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
 } OpCode;
 
 typedef struct {
---
+++ main/vm.h
@@ -0,0 +1,37 @@
+//
+// Created by aaron on 8/10/2024.
+//
+
+/**
+ * Header file for the functions and structs that represent our virtual machine.
+ */
+
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    // Instruction pointer
+    uint8_t* ip;
+
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+#endif
