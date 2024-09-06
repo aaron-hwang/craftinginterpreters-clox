9b026f7d4a68c08de2ef9dc1699e26087e859752
ducc <aaronhwang22@gmail.com>
2024-09-06 16:55:30 -0400

    Added ability to reference and use local variables

--- main/compiler.c
+++ main/compiler.c
@@ -68,6 +68,7 @@ static ParseRule* getRule(TokenType type);
 static void statement();
 static void declaration();
 static uint8_t identifierConstant(Token* name);
+static int resolveLocal(Compiler* compiler, Token* name);
 
 static Chunk* currentChunk() {
     return compilingChunk;
@@ -231,13 +232,22 @@ static void string(bool canAssign) {
 }
 
 static void namedVariable(Token name, bool canAssign) {
-    uint8_t arg = identifierConstant(&name);
+    uint8_t getOp, setOp;
+    int arg = resolveLocal(current, &name);
+    if (arg != -1) {
+        getOp = OP_GET_LOCAL;
+        setOp = OP_SET_LOCAL;
+    } else {
+        arg = identifierConstant(&name);
+        getOp = OP_GET_GLOBAL;
+        setOp = OP_SET_GLOBAL;
+    }
 
     if (canAssign && match(TOKEN_EQUAL)) {
         expression();
-        emitBytes(OP_SET_GLOBAL, arg);
+        emitBytes(setOp, arg);
     } else {
-        emitBytes(OP_GET_GLOBAL, arg);
+        emitBytes(getOp, arg);
     }
 
 }
@@ -377,7 +387,10 @@ static void synchronize() {
 }
 
 static void defineVariable(uint8_t global) {
-    if (current->scopeDepth > 0) return;
+    if (current->scopeDepth > 0) {
+        markInitialized();
+        return;
+    }
     emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
@@ -391,6 +404,21 @@ static bool identifiersEqual(Token* a, Token* b ) {
     return memcmp(a->start, b->start, a->length) == 0;
 }
 
+static int resolveLocal(Compiler* compiler, Token* name) {
+    for (int i = compiler->localCount - 1; i >= 0; i--) {
+        Local* local = &compiler->locals[i];
+        if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Cannot read local variable in it's own initializer");
+            }
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
 static void addLocal(Token name) {
     if (current->localCount == UINT8_COUNT) {
         error("Too many local variables in function. Max is 256");
@@ -398,7 +426,8 @@ static void addLocal(Token name) {
     }
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    // Special value to indicate local's status as uninitialized, for handling special assignment cases
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -426,6 +455,10 @@ static uint8_t parseVariable(const char* message) {
     if (current->scopeDepth > 0) return 0;
 
     return identifierConstant(&parser.previous);
+}
+
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
 }
 
 // Variables declared without an '=' are implcitly declared as NIL
